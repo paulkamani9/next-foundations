c06eaac30fb99e4cdd8d7382159455aa1cad597c
pakmmi <paulkamani9@gmail.com>
2024-07-11 22:47:56 +0400

    html template

--- src/lib/mail.ts
+++ src/lib/mail.ts
@@ -31,19 +31,6 @@ export const sendVerificationEmail = async ({
   token,
 }: SendEmailProps) => {
   try {
-    // const verificationEmail = fs.readFileSync(
-    //   "../email/verification-email.html",
-    //   {
-    //     encoding: "utf-8",
-    //   }
-    // );
-
-    // const html = verificationEmail
-    //   .replace("{name}", name)
-    //   .replace("{companyName}", companyName)
-    //   .replace("{token}", token)
-    //   .replace("{url}", `${process.env.BASE_URL}`);
-
     await transporter.sendMail({
       from: process.env.USER,
       to: email,
@@ -65,7 +52,7 @@ export const sendVerificationEmail = async ({
       display: flex;
       justify-content: center;
       align-items: center;
-      height: 100vh;
+      height: 100%;
     "
   >
     <div
@@ -84,10 +71,10 @@ export const sendVerificationEmail = async ({
         Email Verification
       </h1>
       <p style="font-size: 16px; color: #555; margin-bottom: 20px">
-        Hello {name},
+        Hello ${name},
       </p>
       <p style="font-size: 16px; color: #555; margin-bottom: 20px">
-        You are about to verify your account with {companyName}. You can then
+        You are about to verify your account with ${companyName}. You can then
         login to your new account. Please confirm your decision by clicking the
         button below.
       </p>
@@ -95,7 +82,7 @@ export const sendVerificationEmail = async ({
         Please ignore if you did not request this action.
       </p>
       <a
-        href="{url}/auth/new-verification?token={token}"
+        href="${process.env.BASE_URL}/auth/new-verification?token=${token}"
         style="text-decoration: none"
       >
         <button
@@ -135,21 +122,80 @@ export const sendResetEmail = async ({
   token,
 }: SendEmailProps) => {
   try {
-    const resetEmail = fs.readFileSync("./email", {
-      encoding: "utf-8",
-    });
-
-    const html = resetEmail
-      .replace("{name}", name)
-      .replace("{companyName}", companyName)
-      .replace("{token}", token)
-      .replace("{url}", `${process.env.BASE_URL}`);
-
     await transporter.sendMail({
       from: process.env.USER,
       to: email,
       subject,
-      html,
+      html: `<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Reset Password Email</title>
+  </head>
+  <body
+    style="
+      width: 100%;
+      font-family: Arial, sans-serif;
+      background-color: #f4f4f4;
+      margin: 0;
+      padding: 0;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      height: 100%;
+    "
+  >
+    <div
+      style="
+        background-color: #fff;
+        border-radius: 8px;
+        box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+        width: 100%;
+        max-width: 400px;
+        padding: 20px;
+        box-sizing: border-box;
+        text-align: center;
+      "
+    >
+      <h1 style="font-size: 24px; margin-bottom: 20px; color: #333">
+        Password Reset
+      </h1>
+      <p style="font-size: 16px; color: #555; margin-bottom: 20px">
+        Hello ${name},
+      </p>
+      <p style="font-size: 16px; color: #555; margin-bottom: 20px">
+        You are about to verify your account with ${companyName}. You can then
+        change your password. Please confirm your decision by clicking the
+        button below.
+      </p>
+      <p style="font-size: 16px; color: #555; margin-bottom: 20px">
+        Please ignore if you did not request this action.
+      </p>
+      <a
+        href="${process.env.BASE_URL}/auth/new-password?token=${token}"
+        style="text-decoration: none"
+      >
+        <button
+          type="submit"
+          style="
+            padding: 12px 20px;
+            background-color: #007bff;
+            color: #fff;
+            border: none;
+            border-radius: 4px;
+            font-size: 16px;
+            cursor: pointer;
+            display: inline-block;
+          "
+        >
+          Verify Email
+        </button>
+      </a>
+    </div>
+  </body>
+</html>
+`,
     });
   } catch (error) {
     console.error("Error sending reset email:", error);
@@ -166,16 +212,6 @@ export const sendDeleteEmail = async ({
   token,
 }: SendEmailProps) => {
   try {
-    const deleteEmail = fs.readFileSync("./src/lib/email/delete-email.html", {
-      encoding: "utf-8",
-    });
-
-    const html = deleteEmail
-      .replace("{name}", name)
-      .replace("{companyName}", companyName)
-      .replace("{token}", token)
-      .replace("{url}", `${process.env.BASE_URL}`);
-
     await transporter.sendMail({
       from: process.env.USER,
       to: email,
@@ -219,14 +255,14 @@ export const sendDeleteEmail = async ({
         Hello {name},
       </p>
       <p style="font-size: 16px; color: #555; margin-bottom: 20px">
-        You are about to delete your account with {companyName}. This action is
+        You are about to delete your account with ${companyName}. This action is
         irreversible. Please confirm your decision by clicking the button below.
       </p>
       <p style="font-size: 16px; color: #555; margin-bottom: 20px">
         Please ignore if you did not request this action.
       </p>
       <a
-        href="${process.env.BASE_URL}/delete-account?token={token}"
+        href="${process.env.BASE_URL}/delete-account?token=${token}"
         style="text-decoration: none"
       >
         <button
